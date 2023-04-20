a0ac207f9de447a71ec308b189ddb54c6ed713cd
Jespinola777 <jespinola.777@gmail.com>
2023-04-20 16:32:35 -0600

    done-ish

--- app.js
+++ app.js
@@ -21,26 +21,37 @@ $.get(`https://valorant-api.com/v1/agents`, function (data) {
                 const $body = $('body')
                 for(const agent in data){
                    const agentInfo = data[agent];
-                   console.log(agentInfo)
                    if(agentInfo.isPlayableCharacter===true){
-                    const name = agentInfo.displayName
-                    const background = agentInfo.background
-                    const backgroundGradient = agentInfo.backgroundGradientColors
+                    const name = agentInfo.displayName;
+                    const backgroundGradient = agentInfo.backgroundGradientColors;
                     const gradient = backgroundGradient.map(color => `#${color}`);
-                    const portrait = agentInfo.fullPortrait
-                    const description = agentInfo.description
-                    const displayIcon = agentInfo.displayIcon
-                   
-                    console.log(displayIcon)
-                    const $div = $(".agent")
-                    const $img = $("<img>").addClass("image").attr("src", portrait)
-                    $(".agent-background").css({background: `linear-gradient(${gradient})` });
-                    const $name = $('<p>').text(name).addClass("name")
-                    const $p = $("<p>").text(description).addClass("summary")
-                    $div.append($name)
-                    $div.append($p)
-                    $div.append($img)
-                    $body.append($div)
+                    const portrait = agentInfo.fullPortrait;
+                    const description = agentInfo.description;
+                    const role = agentInfo.role.description;
+                    const className = agentInfo.role.displayName;
+                    const abilities = {};
+                    for(ability in agentInfo.abilities){
+                        abilities[agentInfo.abilities[ability].displayName] = agentInfo.abilities[ability].description;
+                    }
+                    const $ability = $("<div>").text("Abilities").addClass("abilities");
+                    for(ability in abilities){
+                        const list = $("<p>").text(`${ability}: ${abilities[ability]}`) ;
+                        $ability.append(list);
+                    }
+                    const $div = $(".agent");
+                    const $img = $("<img>").addClass("image").attr("src", portrait);
+                    $($body).css({background: `linear-gradient(${gradient})` });
+                    const $name = $('<p>').text(name).addClass("name");
+                    const $p = $("<p>").text(description).addClass("summary");
+                    const $role = $("<div>").text(`Class: ${className}`).addClass("role");
+                    const $roleInfo = $("<p>").text(role);
+                    $role.append($roleInfo);
+                    $div.append($name);
+                    $div.append($p);
+                    $div.append($img);
+                    $div.append($role);
+                    $div.append($ability);
+                    $body.append($div);
                    
                   }    
                }
